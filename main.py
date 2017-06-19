ba02f476425038313814ea6eaf8e422add2c660d
Jim <jimmyorjimi@gmail.com>
2017-06-19 15:14:31 -0400

    Reworked control flow

--- main.py
+++ main.py
@@ -7,58 +7,76 @@ TODO:
 import fetch_data
 from markov_python.cc_markov import MarkovChain
 import lyricize
-from os.path import isfile, isdir, dirname
+from os.path import isfile, isdir
 from os import makedirs
 import directories
 import re
 
 
 curr_dir = directories.get_script_path()
-
-print('\nWebsite codes:\nCODE\tWEBSITE\ndl\tdarklyrics.com\nlf\tlyricsfreak.com\n')
-
-while True:
-    website_code = input('Enter code for website you want to retrieve lyrics from: ')
-    print()
-
-    if website_code:
-        website_code = re.sub('[^a-zA-Z0-9.]+', '', website_code).lower()
-
-        if website_code == 'dl' or\
-                website_code == 'darklyrics' or\
-                website_code == 'darklyrics.com':
-            website_code = 'dl'
-            break
-        elif website_code == 'lf' or\
-                website_code == 'lyricsfreak' or\
-                website_code == 'lyricsfreak.com':
-            website_code = 'lf'
-            break
-        else:
-            print('Unrecognized website code\n')
-
-
-while True:
-    band_name = fetch_data.set_band(website_code)
-    if band_name:
-        url = fetch_data.build_url(website_code, band_name)
-        if fetch_data.url_check(url):
-            break
-    else:
-        continue
-
 filefolder = ''.join((curr_dir, '/original_lyrics'))
 if not isdir(filefolder):
     makedirs(filefolder)
 
-band_name = re.sub('[^a-zA-Z0-9]', '', band_name)
-filepath = ''.join((filefolder, '/', band_name, 'lyrics.txt'))
-if not isfile(filepath):
-    print(filefolder)
-    print(filepath)
-    print('File does not exist. Fetching lyrics from web.\n')
-    lyrics_links = fetch_data.get_links(website_code, url)
-    lyrics = fetch_data.get_lyrics(website_code, lyrics_links, band_name)
+while True:
+    band_name = fetch_data.set_band()
+
+    if band_name:
+        filepath = ''.join((filefolder, '/', band_name[0], 'lyrics.txt'))
+
+        if not isfile(filepath):
+            print('File does not exist. Fetching lyrics from web.\n')
+
+            while True:
+                print('Website codes:\nCODE\tWEBSITE\naz\tazlyrics.com\ndl\tdarklyrics.com\nlf\tlyricsfreak.com\n')
+                website_code = input('Enter code for website you want to retrieve lyrics from: ')
+                print()
+
+                if website_code:
+                    website_code = re.sub('[^a-zA-Z0-9.]+', '', website_code).lower()
+
+                    if website_code in ('az', 'dl', 'lf'):
+                        break
+                    elif 'azlyrics' in website_code:
+                        website_code = 'az'
+                        break
+                    elif 'darklyrics' in website_code:
+                        website_code = 'dl'
+                        break
+                    elif 'lyricsfreak' in website_code:
+                        website_code = 'lf'
+                        break
+                    else:
+                        print('Unrecognized website code\n')
+                        continue
+                else:
+                    continue
+
+            if website_code == 'lf':
+                url = fetch_data.build_url(website_code, band_name[1])
+            else:
+                url = fetch_data.build_url(website_code, band_name[0])
+
+            if website_code == 'az':  # azlyrics.com blocks HEAD requests
+                try:
+                    lyrics_links = fetch_data.get_links(website_code, url)
+                    lyrics = fetch_data.get_lyrics(website_code, lyrics_links, band_name)
+                    break
+                except IOError:
+                    print('Could not connect to url. Make sure band name entered properly, \
+or that the band has a page on the website selected.\n')
+                    continue
+            else:
+                if fetch_data.url_check(url):
+                    lyrics_links = fetch_data.get_links(website_code, url)
+                    lyrics = fetch_data.get_lyrics(website_code, lyrics_links, band_name)
+                    break
+                else:
+                    continue
+        else:
+            break
+    else:
+        continue
 
 lyrics_markov = MarkovChain()
 lyrics_markov.add_file(filepath)
